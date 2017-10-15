4f233b45326a9f0cf99c2fbe4931c2dbf20a823d
Attila Večerek <veat24@gmail.com>
2017-10-15 21:09:28 +0200

    Example of modulo

---
+++ 11_modulo/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 13 % 3
+	fmt.Println(x)
+
+	for i := 1; i < 70; i++ {
+		if i%2 == 1 {
+			fmt.Println("Odd")
+		} else {
+			fmt.Println("Even")
+		}
+	}
+}
