95d774f0100b2ce4910f0d2dbef32aba3fca4b64
Attila Večerek <veat24@gmail.com>
2017-10-15 16:27:26 +0200

    Numeral systems and UTF-8 examples

---
+++ 02_numeral_systems/02_binary/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d - %b\n", 42, 42)
+	//TODO: move this block of code to another directory (logically fitting one)
+	//fmt.Println("Alert (bell): \a")
+	//fmt.Println("Backspace: \b")
+	//fmt.Println("Form feed: \fsomething")
+	//fmt.Println("Carriage return: \rsomething")
+	//fmt.Println("Horizontal tab: \tsomething")
+	//fmt.Println("Vetical tab: \vsomething")
+	//fmt.Println("Mixed tab: \tsomething\vsomething")
+}
